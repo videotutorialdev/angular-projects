8b4943727a0bbf30e9774fb15ad889fbe97cb654
videotutorialdev <12bedevnetwork@gmail.com>
2021-03-28 03:26:48 +0700

    Angular Reactive Forms: Membuat Tampilan Form

--- projects/@app/blog/src/app/app.component.ts
+++ projects/@app/blog/src/app/app.component.ts
@@ -1,32 +1,38 @@
-import { Component, TemplateRef } from '@angular/core';
-import { PopupService } from '@components/popup';
-import { PopupLoginComponent } from './popup-login/popup-login.component';
-
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'ngid-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = '@app/blog';
 
-  constructor(public popupService: PopupService) {}
-
-  public handleShowModal(content: TemplateRef<object>): void {
-    this.popupService.open(content).subscribe((response) => {
-      console.log('Info: Come from response');
-      console.log(response);
-    });
+  public user = {
+    name: 'John Doe', // input text
+    address: 'Jln. Mangga Delima 1', // text area
+    gender: "Male", // input radio
+    age: 22,
+    skills: 1, // input checkbox
+    country: "Indonesia", // input select
+    hobbies: 2, // input select,
   }
 
-  public handleLogin(): void {
-    this.popupService.open(PopupLoginComponent, {
-      user: {
-        id: 1,
-        name: 'John Doe',
-        age: '24',
-        email: 'johndoe@gmail.com'
-      }
-    });
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('Info: Come from ngOnInit');
+  }
+
+  public handleCancel(): void {
+    console.log('Info: Come from handleCancel');
+  }
+
+  public handleReset(): void {
+    console.log('Info: Come from handleReset');
+  }
+
+  public handleSubmit(): void {
+    console.log('Info: Come from handleSubmit');
   }
 }
