b93663b86c07b8088f3950d979c85fd030a623e6
videotutorialdev <12bedevnetwork@gmail.com>
2021-03-28 04:06:57 +0700

    Angular Reactive Forms: Membuat Form Control Untuk Input Radio

--- projects/@app/blog/src/app/app.component.ts
+++ projects/@app/blog/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'ngid-root',
   templateUrl: './app.component.html',
@@ -15,20 +15,35 @@ export class AppComponent implements OnInit {
 
   public formGroup: FormGroup;
 
+  public genderList: Array<{id: number, name: string}>;
+
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.setInitializationState();
     this.setFormGroupState();
     console.log('Info: Come from ngOnInit');
     console.log(this.formGroup);
   }
 
+  private setInitializationState(): void {
+    this.genderList = [
+      { id: 1, name: 'Male' },
+      { id: 2, name: 'Female' },
+    ]
+  }
+
   private setFormGroupState(): void {
     this.formGroup = this.formBuilder.group({
-      name: new FormControl('John Doe'),
-      age: new FormControl(22),
-      address: new FormControl('Pluto')
+      name: ['John Doe'],
+      age: [22],
+      address: ['Pluto'],
+      gender: [{ id: 1, name: 'Male' }]
     });
+  }
+
+  public handleChangeGender(gender: {id: number, name: string}): void {
+    this.formGroup.get('gender').patchValue(gender);
   }
 
   public handleCancel(): void {
