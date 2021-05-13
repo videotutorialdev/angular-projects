58aff0210b88fae2713772d4c4fedbd19ebed5be
Video Tutorial Developer <12bedevnetwork@gmail.com>
2021-05-13 22:35:07 +0700

    add: Angular Card: Membuat Card Di Angular

--- projects/@app/blog/src/app/app.module.ts
+++ projects/@app/blog/src/app/app.module.ts
@@ -1,24 +1,9 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-// library section
-import { ButtonModule } from '@components/button';
-import { FormsModule } from '@components/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { PopupModule } from 'projects/components/popup/src/public-api';
 import { AppComponent } from './app.component';
-import { PopupLoginComponent } from './popup-login/popup-login.component';
 @NgModule({
-  declarations: [AppComponent, PopupLoginComponent],
-  imports: [
-    BrowserModule,
-    ButtonModule,
-    NgbModule,
-    PopupModule,
-    ReactiveFormsModule,
-    FormsModule,
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
